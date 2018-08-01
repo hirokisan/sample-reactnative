ae2f6f84603ab8b998a0348d45f2b49628130435
hirokisan <expsharetoworld@gmail.com>
2018-08-01 16:23:10 +0900

    Use react-native-tab-view

--- App.js
+++ App.js
@@ -7,37 +7,44 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import Swiper from 'react-native-swiper';
+import {Platform, StyleSheet, Text, View, Dimensions} from 'react-native';
+//import ScrollableTabView from 'react-native-scrollable-tab-view';
+//var ScrollableTabView = require('react-native-scrollable-tab-view');
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
 import Sample from './sample';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const FirstRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#ff4081' }]} />
+);
+const SecondRoute = () => (
+  <View style={[styles.container, { backgroundColor: '#673ab7' }]} />
+);
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+      { key: 'third', title: 'Third' },
+    ],
+  };
+
   render() {
     return (
-			<Swiper
-				showsButtons={false}
-				dotColor="#FFFFFF55"
-				activeDotColor="#FFFFFFFF"
-			>
-				<View style={styles.container}>
-					<Text style={styles.welcome}>Welcome to React Native!</Text>
-					<Text style={styles.instructions}>To get started, edit App.js</Text>
-					<Text style={styles.instructions}>{instructions}</Text>
-				</View>
-				<Sample />
-				<View style={styles.container}>
-					<Text style={styles.welcome}>Welcome to React Native!</Text>
-				</View>
-			</Swiper>
+      <TabView
+        navigationState={this.state}
+        renderScene={SceneMap({
+          first: FirstRoute,
+          second: SecondRoute,
+          third: Sample,
+        })}
+        onIndexChange={index => this.setState({ index })}
+        initialLayout={{ width: Dimensions.get('window').width }}
+      />
     );
   }
 }
@@ -47,16 +54,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
