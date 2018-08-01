fc3087795b469c807c07648488a6322577a38bdc
hirokisan <expsharetoworld@gmail.com>
2018-08-01 15:54:35 +0900

    Import other component

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Swiper from 'react-native-swiper';
 
+import Sample from './sample';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -31,6 +33,7 @@ export default class App extends Component<Props> {
 					<Text style={styles.instructions}>To get started, edit App.js</Text>
 					<Text style={styles.instructions}>{instructions}</Text>
 				</View>
+				<Sample />
 				<View style={styles.container}>
 					<Text style={styles.welcome}>Welcome to React Native!</Text>
 				</View>
